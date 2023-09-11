f11fa5eab93781ffb5eeb861398ecf40250054a9
Rae Stanton <raestanton93@gmail.com>
2023-09-11 14:56:05 -0700

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    even_nos = [num for num in num_list if num % 2 == 0]
+    return even_nos
     pass
 
-def make_exclamation(sentence_list):
-    pass
+
+def make_exclamation(sentences):
+    return [sentence + "!" for sentence in sentences]
